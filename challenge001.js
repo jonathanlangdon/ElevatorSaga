761538a381e9d90ee19d0dbb0d2aff3b5664346c
Jonathan Langdon <langdon.jonathan@gmail.com>
2024-06-03 18:33:59 -0400

    // solution to level 1: 15 people in 1 min

---
+++ challenge001.js
@@ -0,0 +1,18 @@
+// solution to level 1: 15 people in 1 min
+
+({
+  init: function (elevators, floors) {
+    var elevator = elevators[0]; // Let's use the first elevator
+
+    // Whenever the elevator is idle (has no more queued destinations) ...
+    elevator.on('idle', function () {
+      // let's go to all the floors (or did we forget one?)
+      elevator.goToFloor(0);
+      elevator.goToFloor(1);
+      elevator.goToFloor(2);
+    });
+  },
+  update: function (dt, elevators, floors) {
+    // We normally don't need to do anything here
+  }
+});
